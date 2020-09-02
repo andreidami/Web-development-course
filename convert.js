d1031ef495af24f143138d0115b95bc9fa832c56
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-09-02 14:37:39 +0300

    homework 6 basic

--- convert.js
+++ convert.js
@@ -3,11 +3,11 @@ function conversionCelsius(f) {
     return(5/9) * (f-32);
 
 }
-document.getElementById("conversion").innerHTML = conversionCelsius(22);
+ document.getElementById("conversion").innerHTML = conversionCelsius(67);
 
 
-
-var x = ((5/9) * (22 -32))
+var f = 67
+var x = ((5/9) * (f -32))
 
 
 console.log(x)
