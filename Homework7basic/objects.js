19bf4580e7362a0e815a0532f246617be320b129
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-09-04 13:24:59 +0300

    add on homework7basic

---
+++ Homework7basic/objects.js
@@ -0,0 +1,11 @@
+var animal = {
+    type: "squirrel",
+    name: "alvin",
+    gut: [ "berries", "flowers"],
+    food: "nuts",
+
+    eat: function() {
+        return this.gut + " " + this.food;
+    }
+}
+     console.log(animal.eat())
