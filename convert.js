d17c293f09e5cfd0347a94615f03fdd406421864
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-09-07 12:39:37 +0300

    Homework6

--- convert.js
+++ convert.js
@@ -3,11 +3,32 @@ function conversionCelsius(f) {
     return(5/9) * (f-32);
 
 }
- document.getElementById("conversion").innerHTML = conversionCelsius(67);
 
-
-var f = 67
-var x = ((5/9) * (f -32))
-
-
+var f = 43
+var x = conversionCelsius(f)
 console.log(x)
+
+var f = 90
+var x = conversionCelsius(f)
+console.log(x)
+
+var f = 9
+var x = conversionCelsius(f)
+console.log(x)
+
+
+function conversionMeters(m) {
+
+    return(m / 0.0254)
+}
+var m = 5
+var i = conversionMeters(m)
+console.log(i)
+
+var m = 8
+var i = conversionMeters(m)
+console.log(i)
+
+var m = 67
+var i = conversionMeters(m)
+console.log(i)
